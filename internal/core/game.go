40c2a7c2bc599215c8ae14b5159810388835f0af
Jakub Mazur <jakmaz.dev@icloud.com>
2025-10-16 00:07:34 +0200

    fix: open proper game

--- internal/core/game.go
+++ internal/core/game.go
@@ -1,10 +1,13 @@
 package core
 
 import (
+	"sort"
+
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/jakmaz/arcade/internal/games/chess"
 	"github.com/jakmaz/arcade/internal/games/snake"
 	"github.com/jakmaz/arcade/internal/games/tetris"
+	"github.com/jakmaz/arcade/internal/games/tictactoe"
 )
 
 type GameInfo struct {
@@ -34,6 +37,12 @@ var Games = map[string]GameInfo{
 		Description: "Strategic board game",
 		NewModel:    func() tea.Model { return chess.New() },
 	},
+	"tictactoe": {
+		ID:          "tictactoe",
+		Name:        "Tic-Tac-Toe",
+		Description: "We all know it",
+		NewModel:    func() tea.Model { return tictactoe.New() },
+	},
 }
 
 func AvailableGames() []GameInfo {
@@ -41,6 +50,9 @@ func AvailableGames() []GameInfo {
 	for _, game := range Games {
 		games = append(games, game)
 	}
+	sort.Slice(games, func(i, j int) bool {
+		return games[i].ID < games[j].ID
+	})
 	return games
 }
 
